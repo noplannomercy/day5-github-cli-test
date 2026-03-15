fe9a565d60012a78c461816c72f2e74cf7a8eea0
vavagirls <vavagirls@gmail.com>
2026-03-15 17:09:55 +0900

    add counter app and utils

---
+++ utils.js
@@ -0,0 +1,17 @@
+// 유틸리티 함수
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// 버그 있음: 나눗셈 시 0 체크 없음
+function divide(a, b) {
+  return a / b;
+}
